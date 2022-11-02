3f3a9f8132c39907de6cb14254f37f2cd2e9e134
Lioz Akirav <lioz.akirav@gmail.com>
2022-11-02 04:23:14 +0200

    fix q2 and add tests

--- 01-python-basics/q3.py
+++ 01-python-basics/q3.py
@@ -1,23 +1,7 @@
 import json
 
-# def sort_data_structure(ds):
-#     match str(type(ds)):
-#         case "<class 'list'>":
-#             ds.sort()
-#             return ds
-#         case "<class 'tuple'>":
-#             sorted_tuple = tuple(sorted(ds))
-#             return sorted_tuple
-#         case "<class 'dict'>":
-#             sorted_dict = dict(sorted(ds.items()))
-#             return sorted_dict
-#         case "<class 'set'>":
-#             sorted_set = dict.fromkeys(sorted(ds))
-#             return sorted_set
-#         case _:
-#             return ds
-
 def convert_to_str_then_sort(ds):
+    """This function gets a data structure, converts it to string, sorts it and then converts it back to its original structure"""
     match str(type(ds)):
         case "<class 'list'>":
             ds = ' '.join(sorted(str(x) for x in ds))
@@ -28,10 +12,9 @@ def convert_to_str_then_sort(ds):
             ds = tuple(ds.split(" "))
             return ds
         case "<class 'dict'>":
+            ds = dict(sorted(ds.items()))
             ds = json.dumps(ds)
-            print(ds)
             ds = json.loads(ds)
-            print(ds)
             return ds
         case "<class 'set'>":
             ds = ' '.join(sorted(str(x) for x in ds))
@@ -41,35 +24,46 @@ def convert_to_str_then_sort(ds):
             return ds
 
 def iterate(ds):
+    """This function iterates over all the elements in the given data structure"""
     match str(type(ds)):
         case "<class 'list'>":
             for element in ds:
                 element = convert_to_str_then_sort(element)
+            return ds
         case "<class 'tuple'>":
             for element in ds:
                 element = convert_to_str_then_sort(element)
+            return ds
         case "<class 'dict'>":
             for key, value in ds.items():
                 ds[key] = convert_to_str_then_sort(value)
+            return ds
         case "<class 'set'>":
             for element in ds:
                 element = convert_to_str_then_sort(element)
+            return ds
         case _:
             return ds
 
 def print_sorted(x):
+    """
+    This function gets as an input a deep data structure, sorting it (in all its levels) and printing it
+
+    >>> print_sorted({"a": 5, "c": 6, "b": [1, 3, 2, 4]})
+    {'a': 5, 'b': ['1', '2', '3', '4'], 'c': 6}
+    >>> print_sorted({"a": 5, "c": 6, "b": [1, 3, "dog", 2, "cat", 4]})
+    {'a': 5, 'b': ['1', '2', '3', '4', 'cat', 'dog'], 'c': 6}
+    >>> print_sorted([5,4,2,0,1])
+    ['0', '1', '2', '4', '5']
+    """
     updated_ds = convert_to_str_then_sort(x)
     ans = iterate(updated_ds)
-    print(type(ans))
     print(ans)
 
 
 def main():
-    # x = {"a": 5, "c": 6, "b": [1, 3, 2, 4]}
-    # x = {"a": 5, "c": 6, "b": [1, 3, "dog", 2, "cat", 4]}
-    x = {"a": 5, "c": [{"b": 1, "a": 2}, {"d": 3, "c": 4}], "b": [1, 3, "dog", 2, "cat", 4]}
-    # x = [[2,1],[4,3],[[6,5],[0,1]]]
-    print_sorted(x)
+    import doctest
+    doctest.testmod()
 
 if __name__ == "__main__":
     main()
