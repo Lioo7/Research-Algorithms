776845d93eb933570fe873e58ebd64d023480244
Lioz Akirav <lioz.akirav@gmail.com>
2022-11-15 20:39:47 +0200

    add q1

--- 03-python-oop/q1.py
+++ 03-python-oop/q1.py
@@ -1,10 +1,16 @@
+import re
+
 def analyze_emails(file_name: str):
     emails = convert_file_to_list(file_name)
+    pattern = '^[a-z\d]+(([._-]{1}[a-z\d]+)+|([a-z\d]*))@[a-z\d-]+\.[a-z]+[a-z]+'
+    for email in emails:
+        if (re.search(pattern, email)):
+            print("Valid!", email)
 
 def convert_file_to_list(file_name: str):
     try:
         with open(file_name, 'r') as file:
-            # stores the emails in a list
+            # stores the emails in a list (remove the /n from the end of each string)
             return [email.rstrip() for email in file]
     except:
         pass
