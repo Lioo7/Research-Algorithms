b648b81d298ededefdcad12b2b731518ed55a18c
Lioz Akirav <lioz.akirav@gmail.com>
2022-11-28 21:27:38 +0200

    finish q1 and add doctest

--- 05-python-design-patterns/homework/q1.py
+++ 05-python-design-patterns/homework/q1.py
@@ -1,10 +1,20 @@
 from itertools import combinations
+import doctest
 
 class bounded_subsets:
+    """
+    An iterator that receives as input a list S of different positive numbers,
+    and some positive number C, and produces a series of all subsets of S, whose sum is at most C
+
+    >>> for s in bounded_subsets([1,2,3], 4):   print(s)
+    [[], [1, 2, 3], (1, 2), (1, 3)]
+    """
+
     def __init__(self, arr: list, bound: int):
         self.arr = sorted(arr)
         self.bound = bound
-        self.ans = [x for x in arr if x <= bound]
+        self.ans = [[]]
+        self.ans.append([x for x in arr if x <= bound])
         self.original_len = len(self.ans)
         self.len = len(self.ans)
 
@@ -15,8 +25,8 @@ class bounded_subsets:
         if self.len > self.original_len:
             raise StopIteration()
 
-        for t in range(2, len(arr)):
-            for combo in combinations(arr, t):
+        for t in range(2, len(self.arr)):
+            for combo in combinations(self.arr, t):
                 if sum(combo) <= self.bound:
                     self.ans.append(combo)
                     self.len += 1
@@ -24,17 +34,6 @@ class bounded_subsets:
         return self.ans
 
 
-def func(arr, bound):
-    ans = [x for x in arr if x <= bound]
-    for t in range(2, len(arr)):
-        for combo in combinations(arr, t): 
-            if sum(combo) <= bound:
-                ans.append(combo)
-    print(ans)
-    return ans
-
 if __name__ == "__main__":
-    arr = [1,2,3]
-    for s in bounded_subsets([1,2,3], 4):
-        print(s)
+    doctest.testmod()
 
